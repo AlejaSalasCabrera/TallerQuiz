cf994ae52e7b29776519be97f454fb9af689aaeb
Alejandra <aleja.20012010@gmial.com>
2017-08-29 14:02:15 -0500

    Initial commit

---
+++ app/src/main/java/com/i012114/tallerquiz/MainActivity.java
@@ -0,0 +1,13 @@
+package com.i012114.tallerquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
